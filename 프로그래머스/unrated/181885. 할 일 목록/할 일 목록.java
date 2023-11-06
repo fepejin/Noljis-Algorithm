75d9a5a2b3ebc226339f17d83f9ae893491813b9
KIMSOLJIN <117498827+noljis@users.noreply.github.com>
2023-11-06 23:29:18 +0900

    [unrated] Title: 할 일 목록, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        int num = 0;
+        int length = todo_list.length;
+        
+        //answer 배열 길이 구하기
+        for(int i = 0; i < length; i++){
+            
+            if(!finished[i]){
+                num++;
+            }
+        }
+        
+        String[] answer = new String[num];
+        num = 0;
+        
+        for(int i = 0;i < length; i++){
+            
+            if(!finished[i]){
+    
+                answer[num] = todo_list[i];
+                num++;
+                
+            }
+            
+        }
+        
+        return answer;
+    }
+}
