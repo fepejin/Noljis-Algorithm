db429aad25131658c8ff2bc5f49d4c84663eab7a
KIMSOLJIN <117498827+noljis@users.noreply.github.com>
2023-11-06 22:25:29 +0900

    Create 또 다른 풀이.java

---
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/또 다른 풀이.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
+        System.out.println(str.repeat(n));
+    }
+}
+
+/*String.repeat(int n) : 문자열을 n번만큼 반복*/
